eb5d78546b8b7e90e88c0a82302121d3a278d1bb
Hardik Madaan <hardikmadaan10c@gmail.com>
2025-07-02 16:26:29 +0000

    More Update

--- models/Inventory.js
+++ models/Inventory.js
@@ -26,7 +26,7 @@ const inventorySchema = new Schema({
   }],
   rawMaterials: [{
     rawMaterialId: {
-      type: Schema.TypesObjectId,
+      type: Schema.Types.ObjectId,
       ref: 'RawMaterial',
       required: true
     },
